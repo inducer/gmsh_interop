3f24f3c056a5a5aa989f9cdb28b388770a823ba2
Andreas Klöckner <inform@tiker.net>
2021-05-22 18:24:12 -0500

    Drop autodoc_typehints from sphinx config
    https://github.com/inducer/modepy/pull/38#issuecomment-846472970

--- doc/conf.py
+++ doc/conf.py
@@ -43,16 +43,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "furo"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-html_theme_options = {
-        }
-
 autoclass_content = "class"
-autodoc_typehints = "description"
 
 intersphinx_mapping = {
         "https://docs.python.org/dev": None,
