47465c9473370fb38d9c918c74a1c88596944d91
Andreas Kloeckner <inform@tiker.net>
2021-06-25 13:53:00 -0500

    Use downloaded shared sphinxconfig

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,11 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+from urllib.request import urlopen
 
-# -- Project information -----------------------------------------------------
+_conf_url = \
+        "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
+with urlopen(_conf_url) as _inf:
+    exec(compile(_inf.read(), _conf_url, "exec"), globals())
 
-project = "gmsh_interop"
-copyright = "2020, Andreas Klöckner"
+copyright = "2020-21, Andreas Klöckner"
 author = "Andreas Klöckner"
 
 ver_dic = {}
@@ -16,43 +14,6 @@ exec(compile(
     "../gmsh_interop/version.py", "exec"), ver_dic)
 version = ".".join(str(x) for x in ver_dic["VERSION"])
 release = ver_dic["VERSION_TEXT"]
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-        "sphinx.ext.autodoc",
-        "sphinx.ext.intersphinx",
-        "sphinx_copybutton",
-]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "furo"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-html_theme_options = {
-        }
-
-autoclass_content = "class"
-autodoc_typehints = "description"
 
 intersphinx_mapping = {
         "https://docs.python.org/dev": None,
