228f8a10ee8b4ecfc58c9fda75158cc8f0df6c98
Alexandru Fikl <alexfikl@gmail.com>
2025-08-02 10:30:09 +0300

    docs: fix missing reference

--- doc/conf.py
+++ doc/conf.py
@@ -4,8 +4,7 @@ from importlib import metadata
 from urllib.request import urlopen
 
 
-_conf_url = \
-        "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
+_conf_url = "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
 with urlopen(_conf_url) as _inf:
     exec(compile(_inf.read(), _conf_url, "exec"), globals())
 
@@ -15,7 +14,15 @@ release = metadata.version("gmsh_interop")
 version = ".".join(release.split(".")[:2])
 
 intersphinx_mapping = {
-        "python": ("https://docs.python.org/dev", None),
-        "numpy": ("https://numpy.org/doc/stable/", None),
-        "meshpy": ("https://documen.tician.de/meshpy", None),
-        }
+    "python": ("https://docs.python.org/dev", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "meshpy": ("https://documen.tician.de/meshpy", None),
+}
+
+sphinxconfig_missing_reference_aliases = {
+    "GmshSource": "obj:gmsh_interop.runner.GmshSource",
+}
+
+
+def setup(app):
+    app.connect("missing-reference", process_autodoc_missing_reference)  # noqa: F821
